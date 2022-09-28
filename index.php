abfc2e4a1a53ff278d6cfdb6f13f30465d250a22
Sourav Choudhary <84680538+souravchoudhary1998@users.noreply.github.com>
2022-09-27 23:16:35 -0400

    added slider

--- index.php
+++ index.php
@@ -5,19 +5,67 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home Page</title>
+    <link rel="shortcut icon" href="./images/favicon-32x32.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/home.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" href="./images/favicon-32x32.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 </head>
 
 <body>
+    <!-- Header -->
     <?php include "./nav.php" ?>
 
-    <?php include "./footer.php"?>
+    <!-- Slider main container -->
+    <main class="image-slider">
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="./images/slider_1.jpg">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./images/slider_2.jpg">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./images/6.jpg">
+                </div>
+            </div>
+
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <?php include "./footer.php" ?>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script>
+    const swiper = new Swiper('.swiper', {
+        spaceBetween: 30,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        }
+    });
+</script>
 
 </html>
---
+++ readme.md
@@ -0,0 +1,20 @@
+## Color
+### black:#4d4751;
+### secondary:#6504b5;
+### footer_links(hover):#a376ff;
+
+## Font
+### Font Family: Roboto
+
+## Folder Structure
+#### │   .gitignore
+#### │   readme.md
+#### │   ... php files
+#### ├───css
+#### │       ... css files
+#### │
+#### ├───images
+#### │       ... images
+#### │
+#### ├───js
+#### │       ... js files
