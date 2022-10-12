907d5c27683dc07f03428d15021a5e5a5af0fa02
jgutierrezo <jgutierrezoa2@gmail.com>
2022-10-11 21:40:19 -0400

    change JS file name

--- login.php
+++ login.php
@@ -56,7 +56,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="js/custom.js"></script>
+    <script src="js/togglePasswordVisibility.js"></script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
