86c61a2467886d2920000fc9a18f4d8ec99cfe5b
Sourav Choudhary <84680538+sourav0098@users.noreply.github.com>
2022-10-16 22:16:32 -0400

    added graphs in dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,12 +15,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
+    <style>
+        .chart{
+            min-width: 50%;
+        }
+        #sidebar{
+            position: sticky;
+            top:0px;
+            height: 100vh;
+        }
+    </style>
 </head>
 
 <body>
-    <?php include "./header_top.php"?>
+    <?php include "./header_top.php" ?>
 
-    <div id="wrapper" style="height:100vh" class="d-flex">
+    <div id="wrapper" class="d-flex">
         <!-- SideBar -->
         <?php include "./sidebar.php" ?>
         <!-- PageContent -->
@@ -29,19 +39,25 @@
                 <button type="button" id="sideBarCollapse" class="btn btn-secondary me-3"><i class="fa-solid fa-bars"></i></button>
                 <h1 class="fw-bold">Dashboard</h1>
             </div>
-            
+            <div class="container d-flex flex-wrap align-items-center">
+                <div class="chart">
+                    <canvas id="chart1"></canvas>
+                </div>
+                <div class="chart pie">
+                    <canvas id="chart2"></canvas>
+                </div>
+                <div class="chart pie">
+                    <canvas id="chart3"></canvas>
+                </div>
+                <div class="chart">
+                    <canvas id="chart4"></canvas>
+                </div>
+            </div>
         </div>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-<script>
-    // Toggling the sidebar menu
-    $(document).ready(function() {
-        $("#sideBarCollapse").on("click", function() {
-            $("#sidebar").toggleClass("active");
-        });
-    });
-</script>
+<script src="./js/dashboard.js"></script>
 
 </html>
